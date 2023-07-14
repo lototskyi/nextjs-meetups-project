c9951da9799f13819542f89f687e08e5d6756e58
alex <o.lototskyi@gmail.com>
2023-07-14 11:30:20 +0300

    detail page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+    return (
+        <MeetupDetail
+            image="https://techboomers.com/wp-content/uploads/2017/11/sites-like-meetup-h.jpg"
+            title="A First Meetup"
+            address="Some address 5, 12345 Some City"
+            descrition="This is a first meetup!"
+        />
+    );
+};
+
+export default MeetupDetails;
