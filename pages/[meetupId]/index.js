781f39ed899ed0098f41c7278fc2fee1bca39d72
alex <o.lototskyi@gmail.com>
2023-07-17 11:14:30 +0300

    using getStaticPaths to pregenerate pages with dynamic path

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,43 @@ const MeetupDetails = () => {
     );
 };
 
+export async function getStaticPaths() {
+
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1'
+                }
+            },
+            {
+                params: {
+                    meetupId: 'm2'
+                }
+            },
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+    // fetch data for a single meetup
+
+    const meetupId = context.params.meetupId;
+
+    console.log(meetupId);
+
+    return {
+        props: {
+            meetupData: {
+                id: meetupId,
+                image: "https://techboomers.com/wp-content/uploads/2017/11/sites-like-meetup-h.jpg",
+                title: "A First Meetup",
+                address: "Some address 5, 12345 Some City",
+                descrition: "This is a first meetup!",
+            },
+        },
+    };
+}
+
 export default MeetupDetails;
