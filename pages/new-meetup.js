6b84e4f1bac82262db8c57ad241c6a48610effd7
alex <o.lototskyi@gmail.com>
2023-07-18 18:12:11 +0300

    removed API calling

--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -7,15 +7,15 @@ const MewMeetupPage = () => {
     const router = useRouter();
 
     async function addMeetupHandler(enteresMeetupData) {
-        const response = await fetch("/api/new-meetup", {
-            method: "POST",
-            body: JSON.stringify(enteresMeetupData),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
+        // const response = await fetch("/api/new-meetup", {
+        //     method: "POST",
+        //     body: JSON.stringify(enteresMeetupData),
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //     },
+        // });
 
-        const data = await response.json();
+        // const data = await response.json();
 
         router.replace("/");
     }
