5d1f98c549f3e5c5ed930adccecb58dcc5ec57ff
alex <o.lototskyi@gmail.com>
2023-07-18 18:14:54 +0300

    removed mongodb API call

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -5,17 +5,19 @@ async function handler(req, res) {
     if (req.method == 'POST') {
         const data = req.body;
 
-        const client = await MongoClient.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.ztlanls.mongodb.net/meetups?retryWrites=true&w=majority`);
-        const db = client.db();
+        // const client = await MongoClient.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.ztlanls.mongodb.net/meetups?retryWrites=true&w=majority`);
+        // const db = client.db();
 
-        const meetupsCollection = db.collection('meetups');
-        const result = await meetupsCollection.insertOne(data);
+        // const meetupsCollection = db.collection('meetups');
+        // const result = await meetupsCollection.insertOne(data);
 
-        console.log(result);
+        // console.log(result);
 
-        client.close();
+        // client.close();
 
-        res.status(201).json({message: 'Meetup inserted!'});
+        // res.status(201).json({message: 'Meetup inserted!'});
+        console.log(data);
+
     }
 }
 
