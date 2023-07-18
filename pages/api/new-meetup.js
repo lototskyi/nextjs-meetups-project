58b4288a815b66a32312586ebf24a349b5c8b6dd
alex <o.lototskyi@gmail.com>
2023-07-18 19:11:13 +0300

    return mongodb connection

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -5,19 +5,17 @@ async function handler(req, res) {
     if (req.method == 'POST') {
         const data = req.body;
 
-        // const client = await MongoClient.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.ztlanls.mongodb.net/meetups?retryWrites=true&w=majority`);
-        // const db = client.db();
+        const client = await MongoClient.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.ztlanls.mongodb.net/meetups?retryWrites=true&w=majority`);
+        const db = client.db();
 
-        // const meetupsCollection = db.collection('meetups');
-        // const result = await meetupsCollection.insertOne(data);
+        const meetupsCollection = db.collection('meetups');
+        const result = await meetupsCollection.insertOne(data);
 
-        // console.log(result);
+        console.log(result);
 
-        // client.close();
+        client.close();
 
         res.status(201).json({message: 'Meetup inserted!'});
-
-        console.log(req);
     }
 }
 
